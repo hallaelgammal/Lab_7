c84907df288b21f70f3d517591eb593b4f22a5d7
hallaelgammal <hallaelgammal6@gmail.com>
2025-11-15 14:45:38 +0200

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +11,16 @@
  *
  * @author Abdallah
  */
-public class Student {
+public class Student extends User {
+    private ArrayList<Integer> enrolledCourses;
+    private HashMap<Integer, ArrayList<String>> progress;
+    
+    public Student(int userId, String username, String email, String passwordHash, ArrayList<Integer> enrolledCourses, HashMap<Integer, ArrayList<String>> progress)
+    { super(userId, "student", username, email, passwordHash);
+    this.enrolledCourses = enrolledCourses;
+    this.progress= progress;
+    
+    }
+    
     
 }
