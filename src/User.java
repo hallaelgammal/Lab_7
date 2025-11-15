8270d3f4f5e61d99e2a98d76e435862d0017c163
Hana-Gamaleldin14 <es.hana.ahmed20231@alexu.edu.eg>
2025-11-15 14:41:38 +0200

    h

---
+++ src/User.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public abstract class User {
+    protected String userId;
+    protected String role;
+    protected String username;
+    protected String email;
+    protected String passwordHash;
+
+    public User(String userId, String role, String userName, String email, String passwordHash) {
+        this.userId = userId;
+        this.role = role;
+        this.username = userName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+    
+}
+
