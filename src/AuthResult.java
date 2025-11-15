26cdf89ee7ee6970912d218b7291c1473f44241c
Hana-Gamaleldin14 <es.hana.ahmed20231@alexu.edu.eg>
2025-11-15 17:27:39 +0200

    Update AuthResult.java

--- src/AuthResult.java
+++ src/AuthResult.java
@@ -8,5 +8,30 @@
  * @author Dell
  */
 public class AuthResult {
-    
+     private boolean success;
+    private String message;
+    private User user;
+
+    public AuthResult(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+        this.user=null;
+    }
+    public AuthResult(boolean success, String message, User user) {
+        this.success = success;
+        this.message = message;
+        this.user =user;
+    }
+    public boolean isSuccess()
+    {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
